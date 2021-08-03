283f685e454f0bb4af693984315c6ef7b7307841
dilaysapmaz <50366875+dilaysapmaz@users.noreply.github.com>
2021-08-03 10:59:41 +0300

    Create PointXY.java

---
+++ PointXY.java
@@ -0,0 +1,15 @@
+/* Author: Dilay SAPMAZ
+ * ID: 041701032
+ * Finish Date: 08.04.2018
+ * Explanation: This code finds the weight of many given points and draws a line. 
+ */
+public class PointXY {
+
+	public int x;  
+	public int y;  
+	
+	PointXY(int x, int y){ //constructor
+		this.x=x;
+		this.y=y;
+	}
+}
